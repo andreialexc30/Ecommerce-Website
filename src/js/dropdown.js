febece3c315b134f09e0d4836b1a93be575c6a90
andreialexc30 <andreialexx30@gmail.com>
2021-04-04 15:50:56 +0300

    deleted dropdown.js

--- src/js/dropdown.js
+++
@@ -1,6 +0,0 @@
-const dropdown = document.querySelector('.dropdown__menu');
-
-dropdown.addEventListener('click', () => {
-    const dropdownMenu = document.getElementById('dropdown');
-    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
-})
